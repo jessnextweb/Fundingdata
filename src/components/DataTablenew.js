4cb98afc7017f9b1dbb085720bd3ff77a7e3831d
Jessica <jess@nextweb.com.au>
2025-02-10 14:31:10 +0530

    Update DataTablenew.js

--- src/components/DataTablenew.js
+++ src/components/DataTablenew.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { Table } from "react-bootstrap";
+// import React from "react";
+// import { Table } from "react-bootstrap";
 
-const DataTablenew = ({ title, data }) => {
-  return (
-    <div className="mb-4">
-      <h4 className="text-center">{title}</h4>
-      <Table striped bordered hover responsive>
-        <thead className="table-dark">
-          <tr>
-            <th>category</th>
-            <th>Beneficiary</th>
-            <th>Amount (SGD)</th>
-            <th>Financial Year</th>
-            <th>Funding Type</th>
-            <th>Quarter</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.category}</td>
-              <td>{item.beneficiary}</td>
-              <td>{item.amount.toLocaleString()}</td>
-              <td>{item.finYear}</td>
-              <td>{item.fundingType}</td>
-              <td>{item.quater}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
-  );
-};
+// const DataTablenew = ({ title, data }) => {
+//   return (
+//     <div className="mb-4">
+//       <h4 className="text-center">{title}</h4>
+//       <Table striped bordered hover responsive>
+//         <thead className="table-dark">
+//           <tr>
+//             <th>category</th>
+//             <th>Beneficiary</th>
+//             <th>Amount (SGD)</th>
+//             <th>Financial Year</th>
+//             <th>Funding Type</th>
+//             <th>Quarter</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {data.map((item, index) => (
+//             <tr key={index}>
+//               <td>{item.category}</td>
+//               <td>{item.beneficiary}</td>
+//               <td>{item.amount.toLocaleString()}</td>
+//               <td>{item.finYear}</td>
+//               <td>{item.fundingType}</td>
+//               <td>{item.quater}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </Table>
+//     </div>
+//   );
+// };
 
-export default DataTablenew;
+// export default DataTablenew;
